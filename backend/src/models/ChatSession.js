9db4ab715075e19abfbde5eef506ee0f7964d2ff
RivalismeUniverse <aceharival@gmail.com>
2025-11-13 19:43:44 +0700

    Update ChatSession.js

--- backend/src/models/ChatSession.js
+++ backend/src/models/ChatSession.js
@@ -56,47 +56,4 @@ module.exports = (sequelize, DataTypes) => {
   
   return ChatSession;
 };
-
-// ChatMessage.js
-const ChatMessage = (sequelize, DataTypes) => {
-  return sequelize.define('ChatMessage', {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true
-    },
-    session_id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      references: {
-        model: 'chat_sessions',
-        key: 'id'
-      }
-    },
-    role: {
-      type: DataTypes.STRING(20),
-      allowNull: false,
-      validate: {
-        isIn: [['user', 'assistant']]
-      }
-    },
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW
-    }
-  }, {
-    tableName: 'chat_messages',
-    timestamps: false,
-    indexes: [
-      { fields: ['session_id'] },
-      { fields: ['created_at'] }
-    ]
-  });
-};
-
-// Export both
-module.exports = { ChatSession, ChatMessage };
+module.exports = { ChatSession }
