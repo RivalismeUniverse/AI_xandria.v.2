2e2166d27b811466b4b76b289ad4d9bb66d36ec6
RivalismeUniverse <aceharival@gmail.com>
2025-11-13 18:22:45 +0700

    Create PersonaAnalytics.js

---
+++ backend/src/models/PersonaAnalytics.js
@@ -0,0 +1,51 @@
+// backend/src/models/PersonaAnalytics.js
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('PersonaAnalytics', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    persona_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: 'personas',
+        key: 'id'
+      }
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    battles_fought: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    battles_won: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    chat_sessions: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    revenue_earned: {
+      type: DataTypes.DECIMAL(18, 4),
+      defaultValue: 0.0
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    }
+  }, {
+    tableName: 'persona_analytics',
+    timestamps: false,
+    indexes: [
+      {
+        unique: true,
+        fields: ['persona_id', 'date']
+      }
+    ]
+  });
+};
