26099db0f7aa69a6fbbd8b0871ccf2be4cd41851
RivalismeUniverse <aceharival@gmail.com>
2025-11-19 10:30:09 +0700

    Update lambda.js

--- backend/src/lambda.js
+++ backend/src/lambda.js
@@ -1,19 +1,7 @@
 const serverless = require('serverless-http');
 const app = require('./server');
 
-// ✅ Wrap Express app for AWS Lambda
-const handler = serverless(app, {
-  binary: ['image/png', 'image/jpeg', 'image/jpg', 'image/gif'],
-  request(request, event, context) {
-    request.context = context;
-    request.event = event;
-  },
-  response(response, event, context) {
-    if (!response.headers['Access-Control-Allow-Origin']) {
-      response.headers['Access-Control-Allow-Origin'] = '*';
-    }
-    return response;
-  }
+// AWS Lambda handler
+exports.handler = serverless(app, {
+  binary: ['audio/*', 'image/*']
 });
-
-module.exports.handler = handler;
