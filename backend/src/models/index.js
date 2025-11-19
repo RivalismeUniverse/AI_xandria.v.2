72f6e77805954d29d980ed84c0f8bd503eb1b97a
RivalismeUniverse <aceharival@gmail.com>
2025-11-19 10:30:52 +0700

    Update index.js

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,105 +1,22 @@
-const { Sequelize } = require('sequelize');
-const config = require('../config/database');
-
-// Initialize Sequelize
-const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  {
-    host: config.host,
-    port: config.port,
-    dialect: config.dialect,
-    logging: config.logging,
-    pool: config.pool,
-    dialectOptions: config.dialectOptions
-  }
-);
+const sequelize = require('../config/database');
 
 // Import models
-const defineUser = require('./User');
-const definePersona = require('./Persona');
-const defineBattle = require('./Battle');
-const defineBattleVote = require('./BattleVote');
-const defineChatSession = require('./ChatSession');
-const defineChatMessage = require('./ChatMessage');
-const defineEvolutionLog = require('./EvolutionLog');
-const defineMarketplaceListing = require('./MarketplaceListing');
-const definePersonaAnalytics = require('./PersonaAnalytics');
+const Persona = require('./Persona');
+const Battle = require('./Battle');
+const User = require('./User');
 
-// Initialize models
-const User = defineUser(sequelize, Sequelize.DataTypes);
-const Persona = definePersona(sequelize, Sequelize.DataTypes);
-const Battle = defineBattle(sequelize, Sequelize.DataTypes);
-const BattleVote = defineBattleVote(sequelize, Sequelize.DataTypes);
-const ChatSession = defineChatSession(sequelize, Sequelize.DataTypes);
-const ChatMessage = defineChatMessage(sequelize, Sequelize.DataTypes);
-const EvolutionLog = defineEvolutionLog(sequelize, Sequelize.DataTypes);
-const MarketplaceListing = defineMarketplaceListing(sequelize, Sequelize.DataTypes);
-const PersonaAnalytics = definePersonaAnalytics(sequelize, Sequelize.DataTypes);
+// Define associations
+Persona.hasMany(Battle, { foreignKey: 'persona1_id', as: 'battlesAsPersona1' });
+Persona.hasMany(Battle, { foreignKey: 'persona2_id', as: 'battlesAsPersona2' });
+Battle.belongsTo(Persona, { foreignKey: 'persona1_id', as: 'persona1' });
+Battle.belongsTo(Persona, { foreignKey: 'persona2_id', as: 'persona2' });
 
-// Define relationships
-const setupAssociations = () => {
-  // User relationships
-  User.hasMany(Persona, { foreignKey: 'creator_id', as: 'personas' });
-  User.hasMany(BattleVote, { foreignKey: 'voter_id', as: 'votes' });
-  User.hasMany(ChatSession, { foreignKey: 'user_id', as: 'chatSessions' });
-  User.hasMany(MarketplaceListing, { foreignKey: 'seller_id', as: 'listings' });
-
-  // Persona relationships
-  Persona.belongsTo(User, { foreignKey: 'creator_id', as: 'creator' });
-  Persona.hasMany(Battle, { foreignKey: 'persona1_id', as: 'battles1' });
-  Persona.hasMany(Battle, { foreignKey: 'persona2_id', as: 'battles2' });
-  Persona.hasMany(ChatSession, { foreignKey: 'persona_id', as: 'chatSessions' });
-  Persona.hasMany(EvolutionLog, { foreignKey: 'persona_id', as: 'evolutionLogs' });
-  Persona.hasMany(MarketplaceListing, { foreignKey: 'persona_id', as: 'listings' });
-  Persona.hasMany(PersonaAnalytics, { foreignKey: 'persona_id', as: 'analytics' });
-
-  // Battle relationships
-  Battle.belongsTo(Persona, { foreignKey: 'persona1_id', as: 'persona1' });
-  Battle.belongsTo(Persona, { foreignKey: 'persona2_id', as: 'persona2' });
-  Battle.belongsTo(Persona, { foreignKey: 'winner_id', as: 'winner' });
-  Battle.hasMany(BattleVote, { foreignKey: 'battle_id', as: 'votes' });
-  Battle.hasMany(EvolutionLog, { foreignKey: 'battle_id', as: 'evolutionLogs' });
-
-  // BattleVote relationships
-  BattleVote.belongsTo(Battle, { foreignKey: 'battle_id', as: 'battle' });
-  BattleVote.belongsTo(User, { foreignKey: 'voter_id', as: 'voter' });
-  BattleVote.belongsTo(Persona, { foreignKey: 'voted_for', as: 'persona' });
-
-  // ChatSession relationships
-  ChatSession.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
-  ChatSession.belongsTo(Persona, { foreignKey: 'persona_id', as: 'persona' });
-  ChatSession.hasMany(ChatMessage, { foreignKey: 'session_id', as: 'messages' });
-
-  // ChatMessage relationships
-  ChatMessage.belongsTo(ChatSession, { foreignKey: 'session_id', as: 'session' });
-
-  // EvolutionLog relationships
-  EvolutionLog.belongsTo(Persona, { foreignKey: 'persona_id', as: 'persona' });
-  EvolutionLog.belongsTo(Battle, { foreignKey: 'battle_id', as: 'battle' });
-
-  // MarketplaceListing relationships
-  MarketplaceListing.belongsTo(Persona, { foreignKey: 'persona_id', as: 'persona' });
-  MarketplaceListing.belongsTo(User, { foreignKey: 'seller_id', as: 'seller' });
-  MarketplaceListing.belongsTo(User, { foreignKey: 'buyer_id', as: 'buyer' });
-
-  // PersonaAnalytics relationships
-  PersonaAnalytics.belongsTo(Persona, { foreignKey: 'persona_id', as: 'persona' });
-};
-
-setupAssociations();
+User.hasMany(Persona, { foreignKey: 'owner_address', sourceKey: 'wallet_address' });
+Persona.belongsTo(User, { foreignKey: 'owner_address', targetKey: 'wallet_address' });
 
 module.exports = {
   sequelize,
-  Sequelize,
-  User,
   Persona,
   Battle,
-  BattleVote,
-  ChatSession,
-  ChatMessage,
-  EvolutionLog,
-  MarketplaceListing,
-  PersonaAnalytics
+  User
 };
